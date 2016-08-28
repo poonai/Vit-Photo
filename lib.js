95ea3a5689efb43618f8d9f373bdf0dda7444a1b
root <rbalajis25@gmail.com>
2016-08-28 14:36:31 +0530

    initial push

---
+++ lib.js
@@ -0,0 +1,47 @@
+var unirest=require('unirest');
+var parser=require('./parser.js');
+var cheerio=require('cheerio');
+var request=require('request')
+var CookieJar = unirest.jar(true);
+var cache=require('memory-cache')
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+
+
+var viewer=function (regno,Sres) {
+
+
+}
+const getCookie=function (cb) {
+  cachedCookie=cache.get('mycookie')
+
+  if(cachedCookie==null){
+    unirest.get("https://vtop.vit.ac.in/student/captcha.asp").headers({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/44.0 (Chrome)'}).jar(CookieJar).end(function(response){
+              const key = Object.keys(response.cookies)[0];
+              Serial=key+'='+response.cookies[key]
+
+              pixMap=parser.getPixelMapFromBuffer(new Buffer(response.body));
+              captcha=parser.getCaptcha(pixMap);
+             unirest.post("https://vtop.vit.ac.in/student/stud_login_submit.asp").headers({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20150101 Firefox/44.0 (Chrome)'}).jar(CookieJar).form({
+                  regno:'14MSE0052',
+                  passwd:'MyWife15Bitch_',
+                  vrfcd:captcha
+              }).end(function(response){
+                cache.put('mycookie',Serial,2*60*1000);
+                 cb(Serial)
+            })
+    })
+  }else {
+    cb(cachedCookie)
+  }
+}
+exports.viewer = function (Sreq,Sres) {
+  getCookie(function (Serial) {
+    regno=Sreq.params.regno.toUpperCase()
+    var jar=request.jar();
+    jar.setCookie(request.cookie(Serial),'https://vtop.vit.ac.in/student/view_photo_2.asp?rgno=14MSE0001');
+    request.get({url:'https://vtop.vit.ac.in/student/view_photo_2.asp?rgno='+regno,jar:jar},function (err,res,body) {
+    request.get({url:'https://vtop.vit.ac.in/student/view_photo_2.asp?rgno='+regno,jar:jar}).pipe(Sres)
+    })
+  })
+
+};
---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const app=express()
+const viewer=require('./lib.js').viewer
+app.get('/',function (req,res,next) {
+  res.json({
+    status:true,
+    description:'vit student photo viewer made with love मोहब्बत அன்பு',
+    author:'SchoolBoy',
+    rep:'https://github.com/sch00lb0y/Vit-Photo',
+    twitter:'https://twitter.com/rbalajis25',
+    example:'http://summa.herokuapp.com/14MSE0052 replace the existing regno with your regno'
+  })
+})
+app.get('/:regno',viewer)
+app.listen(process.env.PORT||2000)
